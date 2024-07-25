cc340569d37f0f8a4dfd768bd03cccdc90037ece
mhamdany <mhamdanydev@gmail.com>
2024-07-25 11:13:55 +0700

    upload pertama

---
+++ src/components/Projects.jsx
@@ -0,0 +1,56 @@
+import { PROJECTS } from "../constants";
+import { motion } from "framer-motion";
+
+const Projects = () => {
+  return (
+    <div className="border-b border-neutral-900 pb-4">
+      <motion.h1
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 0.5 }}
+        className="text-center my-20 text-4xl"
+      >
+        Proyek
+      </motion.h1>
+      <div>
+        {PROJECTS.map((project, index) => (
+          <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
+            <motion.div
+              whileInView={{ opacity: 1, x: 0 }}
+              initial={{ opacity: 0, x: -100 }}
+              transition={{ duration: 1 }}
+              className="w-full md:w-1/4"
+            >
+              <img
+                src={project.image}
+                width={150}
+                height={150}
+                className="mb-6 rounded"
+                alt={project.title}
+              />
+            </motion.div>
+            <motion.div
+              whileInView={{ opacity: 1, x: 0 }}
+              initial={{ opacity: 0, x: 100 }}
+              transition={{ duration: 1 }}
+              className="w-full max-w-xl md:w-3/4 cursor-pointer"
+            >
+              <h6 className="mb-2 font-semibold">{project.title}</h6>
+              <p className="mb-4 text-neutral-400">{project.description}</p>
+              {project.technologies.map((tech, index) => (
+                <span
+                  key={index}
+                  className="mr-2 rounded bg-neutral-900 text-purple-900 py-1 px-2 text-sm font-medium"
+                >
+                  {tech}
+                </span>
+              ))}
+            </motion.div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
